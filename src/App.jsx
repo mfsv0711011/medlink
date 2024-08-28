44c90c5b1932b8c93c1912e1fb41c24176d1ca85
mfsv <m.sheraliyev1011@gmail.com>
2024-08-28 10:45:34 +0500

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+     <h1 className='text-5xl text-teal-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt repudiandae veritatis magnam dolores! Magni provident quasi, voluptate pariatur veniam quidem eius! Ex, exercitationem iusto accusamus id odit natus similique voluptatem!</h1>
+    </>
+  )
+}
+
+export default App
